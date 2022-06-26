0dfa442da8fed86937a8ee324f0203bcc4d43d97
unknown <hannanmalik1999@gmail.com>
2022-06-27 00:23:23 +0200

    Add note summary titles

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -11,7 +11,7 @@ function Sidebar(props) {
                 }`}
                 onClick={() => props.setCurrentNoteId(note.id)}
             >
-                <h4 className="text-snippet">Note {index + 1}</h4>
+                <h4 className="text-snippet">{note.body.split("\n")[0]}</h4>
             </div>
         </div>
     ))
